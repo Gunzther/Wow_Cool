32ead962ae8f62c6244780c1ff4536bb254e69ad
KameriiJ <jenny.sirarat@gmail.com>
2019-04-26 16:26:54 +0700

    first commit

---
+++ src/application/HomeController.java
@@ -0,0 +1,66 @@
+package application;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.*;
+
+public class HomeController {
+	public static Stage stage;
+	public static boolean startStage = false; 
+	
+	@FXML
+	Button start;
+	
+	@FXML
+	public void initialize() {
+		EventHandler<MouseEvent> event1 = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getTarget() == start) {
+					start.setText("CLICK!");
+				}
+			}
+		};
+		
+		EventHandler<MouseEvent> event2 = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getTarget() == start) {
+					start.setText("START");
+				}
+			}
+		};
+		
+		start.addEventHandler(MouseEvent.MOUSE_ENTERED_TARGET, event1);
+		start.addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, event2);
+	}
+	
+	/** Go to mode selection */
+	public void handleStart() {
+		if(Main.stage.isShowing()) Main.stage.close();
+//		else if(startStage && ModeController.stage.isShowing()) ModeController.stage.close();
+		stage = new Stage();
+		try {
+			Parent root = (Parent)FXMLLoader.load(getClass().getResource("TimesTableChoosingUI.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Cool Game Title");
+			stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent event) {
+					System.exit(0);
+				}
+			});
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	
+	public static Stage stage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			stage = primaryStage;
+			Parent root = (Parent)FXMLLoader.load(getClass().getResource("homeUI.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Cool Game Title");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
